f125d86f5af7b70a3b75ad3f89c34d7bc0d64d8b
Raphael Lemieux <1904889+raplemie@users.noreply.github.com>
2021-04-30 15:34:39 -0400

    Add APIM Authorization client - Do not load whole app when handling redirect - Allow use of APIM endpoints.

--- itwin-demo-portal/src/AuthorizationClient.ts
+++ itwin-demo-portal/src/AuthorizationClient.ts
@@ -1,15 +1,16 @@
-import {
-  BrowserAuthorizationCallbackHandler,
-  BrowserAuthorizationClient,
-  BrowserAuthorizationClientConfiguration,
-} from "@bentley/frontend-authorization-client";
+import { BrowserAuthorizationClient, BrowserAuthorizationClientConfiguration } from "@bentley/frontend-authorization-client";
 import { FrontendRequestContext } from "@bentley/imodeljs-frontend";
 
 class AuthorizationClient {
   private static _oidcClient: BrowserAuthorizationClient;
+  private static _apimClient: BrowserAuthorizationClient;
 
   public static get oidcClient(): BrowserAuthorizationClient {
     return this._oidcClient;
+  }
+
+  public static get apimClient(): BrowserAuthorizationClient {
+    return this._apimClient;
   }
 
   public static async initializeOidc(): Promise<void> {
@@ -21,6 +22,7 @@ class AuthorizationClient {
     const clientId = process.env.IMJS_AUTH_CLIENT_CLIENT_ID ?? "";
     const redirectUri = process.env.IMJS_AUTH_CLIENT_REDIRECT_URI ?? "";
     const postSignoutRedirectUri = process.env.IMJS_AUTH_CLIENT_LOGOUT_URI;
+    const apimAuthority = process.env.IMJS_AUTH_CLIENT_APIM_AUTHORITY;
 
     // authority is optional and will default to Production IMS
     const oidcConfiguration: BrowserAuthorizationClientConfiguration = {
@@ -31,23 +33,26 @@ class AuthorizationClient {
       responseType: "code",
     };
 
-    await BrowserAuthorizationCallbackHandler.handleSigninCallback(
-      oidcConfiguration.redirectUri
-    );
-
     this._oidcClient = new BrowserAuthorizationClient(oidcConfiguration);
+    this._apimClient = new BrowserAuthorizationClient({
+      ...oidcConfiguration,
+      authority: apimAuthority,
+    });
   }
 
   public static async signIn(): Promise<void> {
     await this.oidcClient.signIn(new FrontendRequestContext());
+    await this.apimClient.signInSilent(new FrontendRequestContext());
   }
 
   public static async signInSilent(): Promise<void> {
     await this.oidcClient.signInSilent(new FrontendRequestContext());
+    await this.apimClient.signInSilent(new FrontendRequestContext());
   }
 
   public static async signOut(): Promise<void> {
     await this.oidcClient.signOut(new FrontendRequestContext());
+    await this.apimClient.signOut(new FrontendRequestContext());
   }
 }
 
--- itwin-demo-portal/src/index.tsx
+++ itwin-demo-portal/src/index.tsx
@@ -1,12 +1,21 @@
 import "./index.scss";
 
+import { BrowserAuthorizationCallbackHandler } from "@bentley/frontend-authorization-client";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Do not render full application if we are handling OIDC callback
+const redirectUrl = new URL(process.env.IMJS_AUTH_CLIENT_REDIRECT_URI ?? "");
+if (redirectUrl.pathname === window.location.pathname) {
+  BrowserAuthorizationCallbackHandler.handleSigninCallback(
+    redirectUrl.toString()
+  ).catch(console.error);
+} else {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
