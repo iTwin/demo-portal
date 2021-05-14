fd7faf9e4145d59a5495703e2d0bebcf918c3d07
Raphael Lemieux <1904889+raplemie@users.noreply.github.com>
2021-05-14 13:46:20 -0400

    Add Delete iModel option to SelectIModel

--- packages/app/src/routes/SelectIModel.tsx
+++ packages/app/src/routes/SelectIModel.tsx
@@ -1,8 +1,10 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-import { IModelGrid } from "@itwin/imodel-browser";
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import { DeleteIModel } from "@itwin/delete-imodel";
+import { IModelFull, IModelGrid } from "@itwin/imodel-browser";
+import { SvgDelete } from "@itwin/itwinui-icons-react";
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
 
@@ -14,10 +16,41 @@ export const SelectIModel = ({
   accessToken,
   projectId,
   navigate,
-}: IModelRouteProps) => (
-  <IModelGrid
-    accessToken={accessToken}
-    projectId={projectId}
-    onThumbnailClick={(imodel) => navigate?.(`imodel/${imodel.id}`)}
-  />
-);
+}: IModelRouteProps) => {
+  const [iModelToDelete, setIModelToDelete] = React.useState<
+    IModelFull | undefined
+  >();
+  const [refreshKey, setRefreshKey] = React.useState(0);
+  const clearIModelToDelete = () => setIModelToDelete(undefined);
+  const clearIModelToDeleteAndRefreshIModelGrid = () => {
+    clearIModelToDelete();
+    setRefreshKey((key) => (key + 1) % 3);
+  };
+  return (
+    <>
+      <IModelGrid
+        key={refreshKey}
+        accessToken={accessToken}
+        projectId={projectId}
+        onThumbnailClick={(imodel) => navigate?.(`imodel/${imodel.id}`)}
+        iModelOptions={[
+          {
+            key: "delete",
+            icon: <SvgDelete />,
+            onClick: setIModelToDelete,
+            children: "Delete iModel",
+          },
+        ]}
+      />
+      {iModelToDelete && (
+        <DeleteIModel
+          imodel={iModelToDelete}
+          accessToken={accessToken}
+          onClose={clearIModelToDelete}
+          onError={clearIModelToDelete}
+          onSuccess={clearIModelToDeleteAndRefreshIModelGrid}
+        />
+      )}
+    </>
+  );
+};
