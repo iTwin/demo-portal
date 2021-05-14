e6d8ad26cf656b1d8b8ddb4d0ae10f9d68de4ba9
Raphael Lemieux <1904889+raplemie@users.noreply.github.com>
2021-05-14 15:56:44 -0400

    Add regression test so we dont loose the item

--- packages/app/src/routes/SelectIModel.tsx
+++ packages/app/src/routes/SelectIModel.tsx
@@ -3,19 +3,17 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { DeleteIModel } from "@itwin/delete-imodel";
-import { IModelFull, IModelGrid } from "@itwin/imodel-browser";
+import { IModelFull, IModelGrid, IModelGridProps } from "@itwin/imodel-browser";
 import { SvgDelete } from "@itwin/itwinui-icons-react";
 import { RouteComponentProps } from "@reach/router";
 import React from "react";
 
-export interface IModelRouteProps extends RouteComponentProps {
-  projectId?: string;
-  accessToken: string;
-}
+export type IModelRouteProps = RouteComponentProps<IModelGridProps>;
 export const SelectIModel = ({
   accessToken,
   projectId,
   navigate,
+  ...rest
 }: IModelRouteProps) => {
   const [iModelToDelete, setIModelToDelete] = React.useState<
     IModelFull | undefined
@@ -41,11 +39,12 @@ export const SelectIModel = ({
             children: "Delete iModel",
           },
         ]}
+        {...rest}
       />
       {iModelToDelete && (
         <DeleteIModel
           imodel={iModelToDelete}
-          accessToken={accessToken}
+          accessToken={accessToken ?? ""}
           onClose={clearIModelToDelete}
           onError={clearIModelToDelete}
           onSuccess={clearIModelToDeleteAndRefreshIModelGrid}
