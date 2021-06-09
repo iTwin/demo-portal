5dc95576919b9a7da05090a27bb691e465ecec83
kckst8 <6283674+kckst8@users.noreply.github.com>
2021-06-09 15:38:41 -0400

    add config provider and env-config.json

---
+++ packages/app/src/config/ConfigProvider.tsx
@@ -0,0 +1,28 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+import React from "react";
+
+import { DemoPortalConfig } from ".";
+
+export interface ConfigProviderProps extends DemoPortalConfig {
+  children: React.ReactNode;
+}
+
+const ConfigContext = React.createContext<DemoPortalConfig>({});
+
+export const ConfigProvider = (props: ConfigProviderProps) => {
+  const { children, ...rest } = props;
+  return (
+    <ConfigContext.Provider value={rest}>{children}</ConfigContext.Provider>
+  );
+};
+
+export const useConfig = () => {
+  const context = React.useContext(ConfigContext);
+  if (context === undefined) {
+    throw new Error("useConfig must be used within a ConfigContext");
+  }
+  return context;
+};
