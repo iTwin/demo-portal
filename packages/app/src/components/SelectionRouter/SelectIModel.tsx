caef51c9b3216a6e92b820e363e431ce7f3e6d22
spap975 <48142512+spap975@users.noreply.github.com>
2021-06-25 14:18:25 -0400

    fixed merge changes

--- packages/app/src/components/SelectionRouter/SelectIModel.tsx
+++ packages/app/src/components/SelectionRouter/SelectIModel.tsx
@@ -2,12 +2,8 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-import {
-  IModelGrid,
-  IModelGridProps,
-  ProjectFull,
-} from "@itwin/imodel-browser-react";
-import { ButtonGroup, Title } from "@itwin/itwinui-react";
+import { IModelGrid, IModelGridProps } from "@itwin/imodel-browser-react";
+import { ButtonGroup } from "@itwin/itwinui-react";
 import { RouteComponentProps } from "@reach/router";
 import { useFlags } from "launchdarkly-react-client-sdk";
 import React from "react";
@@ -16,6 +12,10 @@ import { useApiPrefix } from "../../api/useApiPrefix";
 import { useCreateIModelAction } from "../IModelCRUDRouter/useCreateIModelAction";
 import { useDeleteIModelAction } from "../IModelCRUDRouter/useDeleteIModelAction";
 import { useEditIModelAction } from "../IModelCRUDRouter/useEditIModelAction";
+import {
+  SynchronizationCardContext,
+  useSynchronizationCards,
+} from "../SynchronizationRouter/useSynchronizationCards";
 import { useSynchronizeIModelAction } from "../SynchronizationRouter/useSynchronizeIModelAction";
 import { useViewIModelAction } from "../ViewRouter/useViewIModelAction";
 import "./SelectIModel.scss";
@@ -57,18 +57,21 @@ export const SelectIModel = ({
         <ButtonGroup>{createIconButton}</ButtonGroup>
       </div>
       <div className="scrolling-tab-content">
-        <IModelGrid
-          key={refreshKey}
-          accessToken={accessToken}
-          projectId={projectId}
-          onThumbnailClick={(imodel) => navigate?.(`imodel/${imodel.id}`)}
-          iModelActions={actions.filter(
-            (action: any) => !hideActions?.includes(action.key as any)
-          )}
-          apiOverrides={{ serverEnvironmentPrefix }}
-          {...rest}
-        />
-        {deleteDialog}
+        <SynchronizationCardContext.Provider value={{ email }}>
+          <IModelGrid
+            useIndividualState={useSynchronizationCards}
+            key={refreshKey}
+            accessToken={accessToken}
+            projectId={projectId}
+            onThumbnailClick={(imodel) => navigate?.(`imodel/${imodel.id}`)}
+            iModelActions={actions.filter(
+              (action) => !hideActions?.includes(action.key as any)
+            )}
+            apiOverrides={{ serverEnvironmentPrefix }}
+            {...rest}
+          />
+          {deleteDialog}
+        </SynchronizationCardContext.Provider>
       </div>
     </div>
   );
