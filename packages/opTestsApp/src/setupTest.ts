796d8546f373dc0c205d8e79e06717d27c9010f9
Arun George <aruniverse@users.noreply.github.com>
2021-07-20 11:17:46 -0400

    add test to check  for unauthorized users, fix viewer test, refactor login accordingly

--- packages/opTestsApp/src/setupTest.ts
+++ packages/opTestsApp/src/setupTest.ts
@@ -5,7 +5,7 @@
 // import fs from "fs";
 import { Browser, BrowserContext, chromium, Page } from "playwright-chromium";
 
-import { SiteUrl, User1 } from "./helpers/config"; // BrowserName
+import { SiteUrl, User, User1 } from "./helpers/config"; // BrowserName
 import { Base, Home, OIDC } from "./helpers/selectors";
 import { elementExists, getBrowserPath } from "./helpers/utils";
 
@@ -17,7 +17,10 @@ let page: Page;
 jest.setTimeout(600000);
 jest.retryTimes(2);
 
-const login = async (): Promise<void> => {
+export const login = async (
+  user: User = User1,
+  waitFor: string = Home.Portal
+): Promise<void> => {
   context = await browser.newContext();
   page = await context.newPage();
 
@@ -25,31 +28,31 @@ const login = async (): Promise<void> => {
     await page.goto(SiteUrl);
     await page.click(Base.signInButton);
     if (await elementExists(page, OIDC.v2.usernameInput)) {
-      await page.fill(OIDC.v2.usernameInput, User1.username);
+      await page.fill(OIDC.v2.usernameInput, user.username);
       await page.click(OIDC.v2.nextInput, { delay: 500 });
-      if (!User1.username.includes("@bentley.com")) {
-        await page.fill(OIDC.v2.passwordInput, User1.password);
+      if (!user.username.includes("@bentley.com")) {
+        await page.fill(OIDC.v2.passwordInput, user.password);
         await page.click(OIDC.v2.signInButton, { delay: 500 });
       } else {
         // SSO
         await page.fill(
           'input[aria-label="firstname.lastname@bentley.com"]',
-          User1.username
+          user.username
         );
         await page.press(
           'input[aria-label="firstname.lastname@bentley.com"]',
           "Enter"
         );
-        await page.fill('input[name="Password"]', User1.password);
+        await page.fill('input[name="Password"]', user.password);
         await page.press('input[name="Password"]', "Enter");
         await page.click('input[type="submit"]');
       }
     } else {
-      await page.fill(OIDC.v1.usernameInput, User1.username);
-      await page.fill(OIDC.v1.passwordInput, User1.password);
+      await page.fill(OIDC.v1.usernameInput, user.username);
+      await page.fill(OIDC.v1.passwordInput, user.password);
       await page.click(OIDC.v1.signInButton, { delay: 500 });
     }
-    await page.waitForSelector(Home.Portal);
+    await page.waitForSelector(waitFor);
   } catch (error) {
     await page.screenshot({ path: "login-failure.png" });
     throw error;
