49e50ec0334f9997860a905b9300a92f6802f79d
Raphael Lemieux <1904889+raplemie@users.noreply.github.com>
2021-08-09 17:20:57 -0400

    Allow display of a named version

--- packages/app/src/routers/ManageVersionsRouter/ManageVersions.tsx
+++ packages/app/src/routers/ManageVersionsRouter/ManageVersions.tsx
@@ -4,22 +4,44 @@
  *
  * This code is for demonstration purposes and should not be considered production ready.
  *--------------------------------------------------------------------------------------------*/
-import { ManageVersions as ACRManageVersions } from "@itwin/manage-versions-react";
-import { RouteComponentProps } from "@reach/router";
+import {
+  ManageVersions as ACRManageVersions,
+  ManageVersionsProps,
+} from "@itwin/manage-versions-react";
+import { RouteComponentProps, useLocation, useNavigate } from "@reach/router";
 import React from "react";
 
 import { useApiPrefix } from "../../api/useApiPrefix";
 import "./ManageVersions.scss";
 
+type ManageVersionsTabs = Required<ManageVersionsProps>["currentTab"];
+
+const CHANGE_TAB_SEARCHSTRING = "?changes";
 export interface VersionsProps extends RouteComponentProps {
+  projectId?: string;
   iModelId?: string;
   accessToken: string;
 }
 export const ManageVersions = ({
   iModelId = "",
+  projectId = "",
   accessToken,
+  navigate: localNavigate,
 }: VersionsProps) => {
+  const location = useLocation();
   const serverEnvironmentPrefix = useApiPrefix();
+  const globalNavigate = useNavigate();
+  const [tab, setTab] = React.useState<ManageVersionsTabs>(
+    location.search === CHANGE_TAB_SEARCHSTRING ? 1 : 0
+  );
+  React.useEffect(() => {
+    const search = tab === 1 ? CHANGE_TAB_SEARCHSTRING : "";
+    if (location.search !== search) {
+      void localNavigate?.(`./${search}`, {
+        replace: true,
+      });
+    }
+  }, [location.search, localNavigate, tab]);
 
   return (
     <div className="manage-versions-with-side-and-splitter">
@@ -27,6 +49,13 @@ export const ManageVersions = ({
         accessToken={accessToken}
         imodelId={iModelId}
         apiOverrides={{ serverEnvironmentPrefix }}
+        onViewClick={(version) =>
+          globalNavigate(
+            `/view/project/${projectId}/imodel/${iModelId}/version/${version.id}`
+          )
+        }
+        onTabChange={setTab}
+        currentTab={tab}
       />
     </div>
   );
